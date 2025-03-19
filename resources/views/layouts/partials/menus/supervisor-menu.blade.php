ee7863b20da07755b539a46fa8e6eb4b72cb1cea
Hadi Fiqih <pratamahadi25@gmail.com>
2025-03-19 16:43:53 +0700

    Add daily report, offers, and customers menu items in supervisor menu

--- resources/views/layouts/partials/menus/supervisor-menu.blade.php
+++ resources/views/layouts/partials/menus/supervisor-menu.blade.php
@@ -62,3 +62,26 @@
         <p>Antrian</p>
     </a>
 </li>
+<!-- Daily Report -->
+<li class="nav-item">
+    <a href="{{ route('sales.reports.index') }}" class="nav-link {{ Request::is('report/daily*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-calendar-day"></i>
+        <p>Laporan Harian</p>
+    </a>
+</li>
+
+<!-- Offers -->
+<li class="nav-item">
+    <a href="{{ route('sales.offers.index') }}" class="nav-link {{ Request::is('sales/offers*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-hand-holding-usd"></i>
+        <p>Penawaran</p>
+    </a>
+</li>
+
+<!-- Customers -->
+<li class="nav-item">
+    <a href="{{ route('customer.index') }}" class="nav-link {{ Request::is('master/customer*') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-users"></i>
+        <p>Pelanggan</p>
+    </a>
+</li>
